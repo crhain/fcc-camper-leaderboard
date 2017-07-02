5dd7ea56591137e633f39046fcc2468292f26c63
Carl Hain <crhain@gmail.com>
2017-07-02 09:42:04 -0500

    Populate hardcoded camper data into a table

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,15 @@
-import { FETCH_DATA } from 'types';
+import { FETCH_DATA } from './types';
 import axios from 'axios';
 
-export default function (){ 
+export function fetchData(){ 
     //this is just returning dumy data
     //will make an api call using axios
     return {
                 type:FETCH_DATA, 
                 payload: [
-                    {name: 'Carl'},
-                    {name: 'Bob'},
-                    {name: 'Carol'}
+                    {name: 'Carl', points30: 10, points: 100},
+                    {name: 'Bob', points30: 5, points: 50},
+                    {name: 'Carol', points30: 25, points: 75}
                 ]
             };  
 }
--- src/components/camper-list.js
+++ src/components/camper-list.js
@@ -1,12 +1,54 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { fetchData } from '../actions/index';
 
-export default class CamperList extends Component{
-    render(){
+class CamperList extends Component{
+    componentWillMount(){
+        this.props.fetchData();
+    }
+    renderCamperRows(){
+        return this.props.campers.map((camper, index)=>{
+            return (
+                <tr key={index}> 
+                    <td>{ camper.name }</td>
+                    <td>{ camper.points30 }</td>
+                    <td>{ camper.points }</td>
+                </tr>
+            );
+        });
+
+    }
+
+    render(){        
         return (
             <div>
                 <h3>Leaderboard</h3>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Camper</th>
+                            <th>Points in Last 30 Days</th>
+                            <th>Total Points</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.renderCamperRows() }
+                    </tbody>
+                    
+                </table>
             </div>
         );
     }
 }
+
+
+function mapStateToProps({ campers }){
+    return { campers };    
+}
+
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({ fetchData }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CamperList);
